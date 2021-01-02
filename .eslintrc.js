1805e308b7bebc5a90934f8a68e0f950eddf4ee2
dayao <961218183@qq.com>
2021-01-02 19:38:17 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+        es6: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [],
+    // required to lint *.vue files
+    plugins: [],
+    // add your custom rules here
+    rules: {}
+}
